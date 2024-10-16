add9055db0cd53f9a6eed27d740ea9009ef17c80
Oh, Sukyeong <osukyeong0109@gmail.com>
2024-10-16 09:36:07 +0900

    five

--- src/f202114011/Main.java
+++ src/f202114011/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("오수경");
 		World.run();
+		World.run2();
 	}
 
 }
--- src/f202114011/World.java
+++ src/f202114011/World.java
@@ -5,4 +5,8 @@ public class World {
 		System.out.println("202114011");
 	}
 
+	public static void run2() {
+		System.out.println("hello world");
+	}
+
 }
